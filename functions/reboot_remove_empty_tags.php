d343add1e4a69621e06c9bbe1f853ca5177602bf
Ömür Yanıkoğlu <hayatbiralem@gmail.com>
2019-04-26 14:08:30 +0300

    Improvements

---
+++ functions/reboot_remove_empty_tags.php
@@ -0,0 +1,62 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit('No direct script access allowed');
+}
+
+if (!function_exists('reboot_remove_empty_tags')) {
+
+    function reboot_remove_empty_tags($html, $tags)
+    {
+        if (empty($html) || empty($tags)) {
+            return $html;
+        }
+
+        if (is_string($tags)) {
+            $tags = [$tags];
+        }
+
+        foreach ($tags as $tag) {
+            if ($tag == 'all') {
+                $pattern = "/<[^\/>]*>([\s]?)*<\/[^>]*>/";
+            } else {
+                $pattern = "/<{$tag}[^>]*>([\s]?)*<\/{$tag}[^>]*>/";
+            }
+
+            $html = preg_replace($pattern, '', $html);
+        }
+
+        return $html;
+    }
+
+}
+
+if (!function_exists('reboot_remove_empty_tags_v2')) {
+
+    function reboot_remove_empty_tags_v2($html, $tags)
+    {
+        if (empty($html) || empty($tags)) {
+            return $html;
+        }
+
+        if (is_string($tags)) {
+            $tags = [$tags];
+        }
+
+        $doc = new DOMDocument();
+        @$doc->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
+
+        foreach ($tags as $tag) {
+            foreach($doc->getElementsByTagName($tag) as $element) {
+                if(trim($element->innertext) == '') {
+                    $element->outertext = '';
+                }
+            }
+        }
+
+        $html = $doc->saveHTML();
+
+        return $html;
+    }
+
+}
--- shortcodes/reboot_smart_link/view.php
+++ shortcodes/reboot_smart_link/view.php
@@ -6,52 +6,51 @@
 
 $description = '';
 
-if(isset($_content) && !empty($_content)) {
-    $description = wpautop($_content);
+if (isset($_content) && !empty($_content)) {
+    $description = reboot_remove_empty_tags( reboot_fix_unclosed_html_tags(wpautop($_content)), 'p' );
 }
 
-if(!isset($title) || empty($title)) {
+if (!isset($title) || empty($title)) {
     $title = '';
 }
 
 $link_attributes = '';
 $link = reboot_vc_parse_link($url);
-if(!empty($link['href'])) {
+if (!empty($link['href'])) {
     $link_attributes = ' ' . $link['attributes'];
 }
 
 
 $classes = ['c-reboot-smart-link'];
 
-if(isset($link_classes) && !empty($link_classes)) {
+if (isset($link_classes) && !empty($link_classes)) {
     $classes[] = trim($link_classes);
 }
 
-if(isset($behavior) && !empty($behavior)) {
+if (isset($behavior) && !empty($behavior)) {
     $classes[] = trim(implode(' ', explode(',', $behavior)));
 }
 
 $classes = implode(' ', $classes);
 
 
-
 ob_start();
 
 ?>
-<div class="c-reboot-smart-link__content">
-    <?php if(!empty($title)) : ?>
-        <h4 class="c-reboot-smart-link__title"><?= $title ?></h4>
-    <?php endif; ?>
+    <div class="c-reboot-smart-link__content">
+        <?php if (!empty($title)) : ?>
+            <h4 class="c-reboot-smart-link__title"><?= $title ?></h4>
+        <?php endif; ?>
 
-    <?php if(!empty($description)) : ?>
-        <div class="c-reboot-smart-link__description"><?= $description ?></div>
-    <?php endif; ?>
-</div>
+        <?php if (!empty($description)) : ?>
+            <div class="c-reboot-smart-link__description"><?= $description ?></div>
+        <?php endif; ?>
+    </div>
 <?php
 
 $output = ob_get_clean();
 
-if(!empty($link_attributes)) {
+if (!empty($link_attributes)) {
     printf('<a class="%s"%s>%s</a>', $classes, $link_attributes, $output);
 } else {
     printf('<div class="%s">%s</div>', $classes, $output);
